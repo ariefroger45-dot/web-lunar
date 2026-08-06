eceba581e3d8d1b2a2a61ada9ee3a97589bbf8e5
ariefroger45-dot <ariefroger45@gmail.com>
2026-08-07 04:58:30 +0700

    add front end cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // alamat front end next js
+    origin: [
+      'http://localhost:3000',
+      'https://web-lunar-go9f.vercel.app', // ganti sesuai URL frontend kamu
+    ],
   });
   await app.listen(3001);
 }
